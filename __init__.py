9432c20143878d0ff8addcf0ec45038f12fdf635
TDgst <129172984+TDgst@users.noreply.github.com>
2025-05-21 10:36:43 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -19,6 +19,17 @@ def encryptage(valeur):
     valeur_bytes = valeur.encode()  # Conversion str -> bytes
     token = f.encrypt(valeur_bytes)  # Encrypt la valeur
     return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str
+  
+@app.route('/decrypt/<string:token_chiffre>')
+def decryptage(token_chiffre):
+    token_bytes = token_chiffre.encode() # Conversion str -> bytes
+    try:
+        valeur_dechiffree_bytes = f.decrypt(token_bytes) # Déchiffre le token
+        valeur_dechiffree_str = valeur_dechiffree_bytes.decode() # Conversion bytes -> str
+        return f"Valeur déchiffrée : {valeur_dechiffree_str}"
+    except Exception as e:
+        # Gère les erreurs potentielles de déchiffrement (ex: token invalide, mauvaise clé)
+        return f"Erreur de déchiffrement : {str(e)}. Assurez-vous que le token est correct et que la clé utilisée pour le chiffrement est la même."
                                                                                                                                                      
 if __name__ == "__main__":
   app.run(debug=True)
